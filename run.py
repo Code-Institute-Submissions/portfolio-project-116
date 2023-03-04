351bffd0d7acc1530a479133d026f716dbae2a51
Humanslaughter <theodorewikstrom@hotmail.com>
2023-03-04 09:06:45 +0000

    Add function for build_ship

--- run.py
+++ run.py
@@ -22,3 +22,31 @@ def generate_board(board):
 board = make_board(5)
 generate_board(board)
 
+"""
+Creates and returns ship coordinates
+by using length of the ship with a random number
+between 2 and the length of the board.
+
+Orientation == 0: ship is horizontal,
+Orientation == 1: ship is vertical.
+"""
+
+
+def build_ship(dims):
+    ship_len = random.randint(2, dims)
+    orientation = random.randint(0, 1)
+
+    if orientation == 0:
+        ship_row = [random.randint(0, dims - 1)] * ship_len
+        col = random.randint(0, dims - ship_len)
+        ship_col = list(range(col, col + ship_len))
+        coords = tuple(zip(ship_row, ship_col))
+    else:
+        ship_col = [random.randint(0, dims - 1)] * ship_len
+        row = random.randint(0, dims - ship_len)
+        ship_row = list(range(row, row + ship_len))
+        coords = tuple(zip(ship_row, ship_col))
+    return list(coords)
+
+
+ship = build_ship(5); ship
