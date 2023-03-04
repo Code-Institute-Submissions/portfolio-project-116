cbb62c946abb5aacecc798ba363fde4dced0ca35
Humanslaughter <theodorewikstrom@hotmail.com>
2023-03-04 09:35:18 +0000

    Add some changes to code

--- run.py
+++ run.py
@@ -1,17 +1,15 @@
 import random
 
-print("=" * 39)
+print("=" * 35)
 print(" Welcome to a game of Battleships!")
-print(" Guess a row and column between 0 & 4.")
-print("=" * 39)
+print(" Guess a row and column between\n 1 and 5.")
+print("=" * 35)
 
 """
 Creates a board in a square, with dims. 
 """
-
-
 def make_board(dims):
-    return [["O" for count in range(dims)] for count in range(dims)]
+    return [["*" for count in range(dims)] for count in range(dims)]
 
 
 def generate_board(board):
@@ -30,8 +28,6 @@ between 2 and the length of the board.
 Orientation == 0: ship is horizontal,
 Orientation == 1: ship is vertical.
 """
-
-
 def build_ship(dims):
     ship_len = random.randint(2, dims)
     orientation = random.randint(0, 1)
@@ -51,16 +47,14 @@ def build_ship(dims):
 
 ship = build_ship(5); ship
 
+
 """
 Function for player guesses
 """
-
-
 def player_guess():
-    print("-" * 38)
-    row = int(input("Guess row:"))-1
-    col = int(input("Guess column:"))-1
-    print("-" * 38)
+    row = int(input("Guess row:\n")) - 1
+    col = int(input("Guess column:\n")) - 1
+    print("=" * 38)
     return (row, col)
 
 
@@ -69,15 +63,13 @@ x = player_guess(); x
 
 def update_board(guess, board, ship, guesses):
     if guess in guesses:
-        print("-" * 38)
         print("You've already guessed that!")
-        print("-" * 38)
+        print("=" * 38)
         return board
     guesses.append(guess)
     if guess in ship:
-        print("-" * 38)
         print("It's a hit!")
-        print("-" * 38)
+        print("=" * 38)
         """
         'X' = hit
         """
@@ -87,23 +79,22 @@ def update_board(guess, board, ship, guesses):
         """
         ship.remove(guess)
         return board
-    print("-" * 38)
-    print("Miss!")
-    print("-" * 38)
+    print("You missed!")
+    print("=" * 38)
     return board
 
-    guesses = []
-    our_guess = player_guess()
-    board = update_board(our_guess, board, ship, guesses)
 
+guesses = []
+our_guess = player_guess()
+board = update_board(our_guess, board, ship, guesses)
+    
 
 generate_board(board)
+
 
 """
 Main function to call all code.
 """
-
-
 def main():
     board = make_board(5)
     ship = build_ship(5)
@@ -111,10 +102,9 @@ def main():
     while len(ship) > 0:
         board = update_board(player_guess(), board, ship, guesses)
         generate_board(board)
-        print("-" * 38)
-        print("You sunk my ship!")
-        print("-" * 38)
-        return
+    print("You sunk my ship!")
+    print("=" * 38)
+    return
 
 
 main()
