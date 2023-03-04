95cef4a375d2d4d101415c016c70fc7856770deb
Humanslaughter <theodorewikstrom@hotmail.com>
2023-03-04 09:05:11 +0000

    Add function for board

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+print("=" * 39)
+print(" Welcome to a game of Battleships!")
+print(" Guess a row and column between 0 & 4.")
+print("=" * 39)
+
+"""
+Creates a board in a square, with dims. 
+"""
+
+
+def make_board(dims):
+    return [["O" for count in range(dims)] for count in range(dims)]
+
+
+def generate_board(board):
+    for i in board:
+        print(*i)
+
+
+board = make_board(5)
+generate_board(board)
+
